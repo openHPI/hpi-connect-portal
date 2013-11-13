8ce6c3cebd89495c571f8ce2d9d71fede0be8d11
Judith Hartmann <judith.hartmann@student.hpi.uni-potsdam.de>
2013-11-13 14:48:47 +0100

    check presence of attributes in job_offers, #6

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,2 +1,4 @@
 class JobOffer < ActiveRecord::Base
+	validates :title, :description, :chair, :start_date, :time_effort, :compensation, presence: true
+
 end
--- spec/models/job_offer_spec.rb
+++ spec/models/job_offer_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe JobOffer do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "does not create a joboffer if attributes are not set" do
+		beforeCount = JobOffer.count
+		JobOffer.create
+		afterCount = JobOffer.count
+		assert_equal(beforeCount, afterCount)
+	end
+
+	it "does create a joboffer if all required attributes are set" do
+		beforeCount = JobOffer.count
+		JobOffer.create(title:"Awesome Job", description: "Develope a website", chair:"Epic", start_date: Date.new(2013,11,1), compensation: 10.5, time_effort: 9)
+		afterCount = JobOffer.count
+		assert_equal(beforeCount+1, afterCount)
+	end
 end
+
+
