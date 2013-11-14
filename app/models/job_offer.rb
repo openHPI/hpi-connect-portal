5b1173d51b59f0d11e2def094ebc280305ca066a
Benjamin Reissaus <benjamin.reissaus@gmail.com>
2013-11-14 01:27:16 +0100

    finished basic implementation of date and chair filter

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,7 +1,11 @@
 class JobOffer < ActiveRecord::Base
 
 	def self.sort(order_attribute) 
-		order(order_attribute)
+		if order_attribute == "date"
+			order(:start_date)
+		elsif order_attribute == "chair"
+			order(:chair)
+		end
 	end
 
 end
