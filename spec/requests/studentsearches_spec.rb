a75dc5c9691cef64b903b86c2c81b2d01067c11b
Frank Blechschmidt <frank.blechschmidt@student.hpi.uni-potsdam.de>
2013-11-15 16:24:52 +0100

    fixed studentsearch test

--- spec/requests/studentsearches_spec.rb
+++ spec/requests/studentsearches_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
 describe "Studentsearches" do
-    let(:student) {FactoryGirl.create(:student,
-        :first_name => 'Alexander',
-        :last_name  => 'Zeier',
-        :education => 'accenture'
-    )}
+    
+    before :each do   
+        @student = FactoryGirl.create(:student,
+            :first_name => 'Alexander',
+            :last_name  => 'Zeier',
+            :education => 'accenture'
+        )
+    end
 
     it 'when searched for full-text attribute' do
         visit studentsearch_index_path
         fill_in 'q', :with => 'CC'
         find('input[type="submit"]').click
-        expect(page).to have_content "#{student.first_name} #{student.last_name}"
+        expect(page).to have_content "#{@student.first_name} #{@student.last_name}"
     end
 
 end
