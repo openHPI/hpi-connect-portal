d4b66af06cce015df50f0b0abcb02ceb95f1d1e0
irruputuncu <pasrein@web.de>
2013-11-15 13:18:59 +0100

    Working open id login with hacked email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,8 @@
 class SessionsController < Devise::SessionsController
     skip_before_filter :verify_authenticity_token
+    before_filter :update_sanitized_params
+
+    def update_sanitized_params
+        devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:identity_url)}
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,34 +2,39 @@
 #
 # Table name: users
 #
-#  id                     :integer          not null, primary key
-#  email                  :string(255)      default(""), not null
-#  encrypted_password     :string(255)      default(""), not null
-#  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0)
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
-#  created_at             :datetime
-#  updated_at             :datetime
-#  identity_url           :string(255)
-#  is_student             :boolean
-#  lastname               :string(255)
-#  firstname              :string(255)
+#  id                  :integer          not null, primary key
+#  email               :string(255)      default(""), not null
+#  remember_created_at :datetime
+#  sign_in_count       :integer          default(0)
+#  current_sign_in_at  :datetime
+#  last_sign_in_at     :datetime
+#  current_sign_in_ip  :string(255)
+#  last_sign_in_ip     :string(255)
+#  created_at          :datetime
+#  updated_at          :datetime
+#  identity_url        :string(255)
+#  is_student          :boolean
+#  lastname            :string(255)
+#  firstname           :string(255)
 #
+
+#this method will be deprecated as soon as open id returns the email
+def email_from_identity_url(identity_url)
+    username = identity_url.reverse[0..identity_url.reverse.index('/')-1].reverse
+    return username + '@student.hpi.uni-potsdam.de'
+end
 
 class User < ActiveRecord::Base
     # Include default devise modules. Others available are:
     # :token_authenticatable, :confirmable,
     # :lockable, :timeoutable and :omniauthable
-    devise :registerable, :rememberable, :trackable, 
-        :validatable, :openid_authenticatable
+    devise :rememberable, :trackable, :openid_authenticatable
+
+    validates :email, uniqueness: { case_sensitive: false }
+    validates :identity_url, uniqueness: true
 
     def self.build_from_identity_url(identity_url)
-        User.new(identity_url: identity_url)
+        User.new(identity_url: identity_url, email: email_from_identity_url(identity_url))
     end
     
     def self.openid_required_fields
@@ -45,11 +50,11 @@ class User < ActiveRecord::Base
 
             case key.to_s
                 when 'first', 'http://axschema.org/namePerson/first'
-                    self.firstname = value
+                    self.firstname = value unless value.blank?
                 when 'last', 'http://axschema.org/namePerson/last'
-                    self.lastname = value
+                    self.lastname = value unless value.blank?
                 when 'email', 'http://axschema.org/contact/email'
-                    self.email = value
+                    self.email = value unless value.blank?
                 else
                     logger.error "Unknown OpenID field: #{key}"
             end
