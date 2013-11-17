4e399bce7e2c23d569594b0348fc1424ce2e4328
alexanderernst <leverkusener77@googlemail.com>
2013-11-17 19:12:27 +0100

    seeded JobOffers
    by running rake db:seed all JobOffers are deleted and 2 fake Jobs are entered.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-ProgrammingLanguage.create(name: "C")
-ProgrammingLanguage.create(name: "C++")
-ProgrammingLanguage.create(name: "Java")
-ProgrammingLanguage.create(name: "Javascript")
-ProgrammingLanguage.create(name: "Python")
-ProgrammingLanguage.create(name: "Ruby")
-ProgrammingLanguage.create(name: "Smalltalk")
+def createJobOffers
+	JobOffer.delete_all
+	JobOffer.create(title: "tele-Task developer", description: "The Job includes the development of new features for tele-Task", 
+		chair:"Internet-Technologien und -Systeme", start_date: Date.new(2013,12,1), time_effort: 10, compensation: 11.50,
+		:programming_language_ids => [6])
+	JobOffer.create(title: "Touch floor", description: "The student extends the functionality of the touch floor.", 
+		chair:"Human Computer Interaction", start_date: Date.new(2013,12,3), time_effort: 6, compensation: 11.50,
+		:programming_language_ids => [1,2])
+
+end
+
+def createProgrammingLanguages
+	ProgrammingLanguage.create(name: "C")
+	ProgrammingLanguage.create(name: "C++")
+	ProgrammingLanguage.create(name: "Java")
+	ProgrammingLanguage.create(name: "Javascript")
+	ProgrammingLanguage.create(name: "Python")
+	ProgrammingLanguage.create(name: "Ruby")
+	ProgrammingLanguage.create(name: "Smalltalk")
+end
+
+
+createProgrammingLanguages
+createJobOffers
