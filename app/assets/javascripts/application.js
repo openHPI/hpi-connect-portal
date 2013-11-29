d4e06e790eed031d892e45fc99defe125cd15fe2
Jasper Bryan <jbmorgen@facebook.com>
2013-11-29 16:19:45 +0100

    removed jquery.ui for performance

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.all
+//= no require jquery.ui.all
 //= require jquery.turbolinks
 //= require bootstrap/bootstrap
 //= require turbolinks
