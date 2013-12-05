56d80f6d252dff9053809981800b212788ef15de
Lars Rückert <lars.rueckert@student.hpi.uni-potsdam.de>
2013-12-05 14:39:29 +0100

    #72 completed - implemented filter function

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -106,30 +106,40 @@ class JobOffer < ActiveRecord::Base
     end
 
     def self.filter_programming_languages(programming_language_ids)
-
-        filtered_jobs = []
-        all.each do | job_offer |
-            prog_lang_id_copy = Array.new programming_language_ids
-
-            temp = joins(:programming_languages).where('job_offers.id=?', job_offer.id).select("programming_languages.id")
-            logger.warn(temp)
-            temp.each do | job_prog_tuple |
-                logger.warn("tuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuple")
-                logger.warn(job_prog_tuple.id.to_s)
-                prog_lang_id_copy.delete(job_prog_tuple.id.to_s)
-            end 
-
-            if prog_lang_id_copy.empty?
-                filtered_jobs << job_offer
+        if programming_language_ids.blank?
+            all
+        else
+            jobs_filter = []
+            all.each do | job_offer |
+                prog_lang_id_copy = Array.new programming_language_ids
+                temp = joins(:programming_languages).where('job_offers.id=?', job_offer.id).select("programming_languages.id")
+                temp.each do | job_prog_tuple |
+                    prog_lang_id_copy.delete(job_prog_tuple.id.to_s)
+                end 
+                if prog_lang_id_copy.empty?
+                    jobs_filter << job_offer.id
+                end
             end
-
+            all.where(id: jobs_filter)
         end
-        filtered_jobs
-       # programming_language_ids.blank? ? all : includes(:job_offers_programming_languages).where('programming_language_id in ?', programming_language_ids)
     end
 
     def self.filter_languages(language_ids)
-        # programming_language_ids.blank? ? all : includes(:programming_languages).where('programming_language_id @> ?', programming_language_ids)
-        all
+        if language_ids.blank?
+            all
+        else
+            jobs_filter = []
+            all.each do | job_offer |
+                lang_id_copy = Array.new language_ids
+                temp = joins(:languages).where('job_offers.id=?', job_offer.id).select("languages.id")
+                temp.each do | job_lang_tuple |
+                    lang_id_copy.delete(job_lang_tuple.id.to_s)
+                end 
+                if lang_id_copy.empty?
+                    jobs_filter << job_offer.id
+                end
+            end
+            all.where(id: jobs_filter)
+        end
     end
 end
