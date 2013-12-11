5b5a444d56e128ba4c640a9a454213404848914d
Judith Hartmann <judith.hartmann@student.hpi.uni-potsdam.de>
2013-12-11 19:02:28 +0100

    fixed viewtest

--- spec/views/students/index.html.erb_spec.rb
+++ spec/views/students/index.html.erb_spec.rb
@@ -35,6 +35,7 @@ describe "students/index" do
   end
 
   it "renders a list of students" do
+    view.stub(:will_paginate)
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "First Name".to_s, :count => 2
