85a7fceeff57a7a1962be94092d5ec4bb255fc95
Tim Specht <tim@tim-specht.de>
2013-12-22 18:44:52 +0100

    more chair tests

--- spec/features/chair_pages_spec.rb
+++ spec/features/chair_pages_spec.rb
@@ -1,31 +1,52 @@
 require 'spec_helper'
 
-
 describe "the chair page" do
 
-  before(:each) do
-    @chair = FactoryGirl.create(:chair, name:"EPIC")
-    @user = FactoryGirl.create(:user)
+  subject { page }
 
-    @deputy = @chair.deputy
+  let(:chair) { FactoryGirl.create(:chair, name:"EPIC") }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:deputy) { chair.deputy }
+
+  before do
+    @running = FactoryGirl.create(:job_status, name: 'running')
+    @open = FactoryGirl.create(:job_status, name: 'open')
+
+    @job_offer_open = FactoryGirl.create(:job_offer, chair: chair, status: FactoryGirl.create(:job_status, name: 'open'))
+    @job_offer_running = FactoryGirl.create(:job_offer, chair: chair, status: FactoryGirl.create(:job_status, name: 'running'))
+
+    visit chair_path(chair)
   end
 
-  it "should show the basic information of the chair" do
+  describe "should show the basic information of the chair" do
+    it { should have_content(chair.name) }
+    it { should have_content(chair.description) }
+    it { should have_content(chair.head_of_chair) }
+    it { should have_content(chair.deputy.email) }
   end
 
-  it "should display open job offers for the chair" do
-  end
+  describe "being the deputy or an admin" do
+      before do
+        login_as(deputy, :scope => :user)
+      end
 
-  it "should display running jobs for the chair" do
-  end
+      it { should have_link 'Edit' }
+    end
 
-  # it "should include all jobs currently available" do
-  #   visit job_offers_path
-  #   page.should have_content(
-  #     @job_offer_1.title,
-  #     @job_offer_2.title,
-  #     @job_offer_3.title
-  #   )
-  # end
+  describe "shows job offers for the chair" do
+
+    it { should have_content('Open') }
+    it { should have_content('Running') }
+
+    it { should have_content(@job_offer_open.title) }
+    it { should have_content(@job_offer_running.title) }
+
+    it { should have_content(@job_offer_open.start_date) }
+    it { should have_content(@job_offer_running.start_date) }
+
+    it { should have_content(@job_offer_open.chair.name) }
+    it { should have_content(@job_offer_running.chair.name) }
+
+  end
 
  end
