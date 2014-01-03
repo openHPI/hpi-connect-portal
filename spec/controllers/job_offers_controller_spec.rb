99dfd45cc083d2ad9da220a17cea6e61359782d7
Johannes Koch <johannes.koch@student.hpi.uni-potsdam.de>
2014-01-03 11:47:53 +0100

    tests and controller functionality for issue #119

--- spec/controllers/job_offers_controller_spec.rb
+++ spec/controllers/job_offers_controller_spec.rb
@@ -25,12 +25,12 @@ describe JobOffersController do
   # adjust the attributes here as well.
   let(:assigned_student) { FactoryGirl.create(:user) }
   let(:chair) { FactoryGirl.create(:chair, name: "Chair") }
-  let(:responsible_user) { FactoryGirl.create(:user, chair: chair) }
-  let(:completed) {FactoryGirl.create(:job_status, name: "completed")}
+  let(:responsible_user) { FactoryGirl.create(:user, chair: chair, role: FactoryGirl.create(:role, :name => "Research Assistant")) }
+  let(:completed) {FactoryGirl.create(:job_status, :completed)}
   let(:valid_attributes) {{ "title"=>"Open HPI Job", "description" => "MyString", "chair_id" => chair.id, "start_date" => Date.new(2013,11,1),
-                        "time_effort" => 3.5, "compensation" => 10.30, "status" => FactoryGirl.create(:job_status, :name => "open")} }
+                        "time_effort" => 3.5, "compensation" => 10.30, "status" => FactoryGirl.create(:job_status, :open)} }
   let(:valid_attributes_status_running) {{"title"=>"Open HPI Job", "description" => "MyString", "chair_id" => chair.id, "start_date" => Date.new(2013,11,1),
-                        "time_effort" => 3.5, "compensation" => 10.30, "status" => FactoryGirl.create(:job_status, :name => "running"), "assigned_student_id" => assigned_student.id, "responsible_user_id" => responsible_user.id }}
+                        "time_effort" => 3.5, "compensation" => 10.30, "status" => FactoryGirl.create(:job_status, :running), "assigned_student_id" => assigned_student.id, "responsible_user_id" => responsible_user.id }}
   let(:valid_attributes_status_completed) {{"title"=>"Open HPI Job", "description" => "MyString", "chair_id" => chair.id, "start_date" => Date.new(2013,11,1),
                         "time_effort" => 3.5, "compensation" => 10.30, "status" => completed, "assigned_student_id" => assigned_student.email}}
 
@@ -39,6 +39,13 @@ describe JobOffersController do
   # in order to pass any filters (e.g. authentication) defined in
   # job_offersController. Be sure to keep this updated too.
   let(:valid_session) { {} }
+
+  before(:all) do 
+    FactoryGirl.create(:job_status, :pending)
+    FactoryGirl.create(:job_status, :open)
+    FactoryGirl.create(:job_status, :running)
+    FactoryGirl.create(:job_status, :completed)
+  end
 
   before(:each) do
       @epic = FactoryGirl.create(:chair, name:"EPIC")
@@ -142,7 +149,7 @@ describe JobOffersController do
     end
 
     it "marks jobs as completed if the user is research assistant of the chair" do 
-      completed = FactoryGirl.create(:job_status, name: "completed")
+      completed = FactoryGirl.create(:job_status, :completed)
       sign_in FactoryGirl.create(:user, role: FactoryGirl.create(:role, name: 'Research Assistant', level: 2), chair: @job_offer.chair)
       
       get :complete, {:id => @job_offer.id}
@@ -159,7 +166,7 @@ describe JobOffersController do
     before(:each) do
       chair.update(deputy: deputy)
       @job_offer = JobOffer.create! valid_attributes
-      @job_offer.update(chair: chair, status: FactoryGirl.create(:job_status, name: "pending"))
+      @job_offer.update(chair: chair, status: FactoryGirl.create(:job_status))
     end
 
     it "prohibits user to accept job offers if he is not the deputy" do
@@ -183,7 +190,7 @@ describe JobOffersController do
     before(:each) do
       chair.update(deputy: deputy)
       @job_offer = JobOffer.create! valid_attributes
-      @job_offer.update(chair: chair, status: FactoryGirl.create(:job_status, name: "pending"))
+      @job_offer.update(chair: chair, status: FactoryGirl.create(:job_status))
     end
 
     it "prohibits user to decline job offers if he is not the deputy" do
@@ -201,6 +208,40 @@ describe JobOffersController do
       }.to change(JobOffer, :count).by(-1)
       response.should redirect_to(job_offers_path)
     end 
+  end
+
+  describe "GET reopen" do
+    describe "with valid params" do
+
+      before(:each) do
+        sign_in responsible_user
+        @job_offer = JobOffer.create! valid_attributes
+        @job_offer.update(chair: chair, status: FactoryGirl.create(:job_status, :running), responsible_user: responsible_user)
+      end
+
+      it "assigns a new job_offer as @job_offer" do
+        get :reopen, {:id => @job_offer}, valid_session
+        assigns(:job_offer).should be_a_new(JobOffer)
+        response.should redirect_to(edit_job_offer_path)
+      end
+
+      it "has same values as the original job offer" do
+        get :reopen, {:id => @job_offer}, valid_session
+        reopend_job_offer = assigns(:job_offer)
+        expected_attr = [:description, :title, :time_effort, :compensation, :room_number, :chair_id, :responsible_user_id]        
+
+        reopend_job_offer.attributes.with_indifferent_access.slice(expected_attr).should eql(@job_offer.attributes.with_indifferent_access.slice(expected_attr))
+        reopend_job_offer.start_date.should be_nil
+        reopend_job_offer.end_date.should be_nil
+        reopend_job_offer.assigned_student_id.should be_nil
+      end
+
+      it "is pending and old job offer changes to completed" do
+        get :reopen, {:id => @job_offer}, valid_session
+        reopend_job_offer = assigns(:job_offer)
+        JobOffer.find(@job_offer).status.should eql(completed)
+      end
+    end
   end
 
   describe "POST create" do
