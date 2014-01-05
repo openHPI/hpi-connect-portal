6047445b4dd0aca0a7c842758caf02afd4fb848a
Tim <tim@tim-specht.de>
2014-01-05 11:36:18 +0100

    refactored studentsearch_controller a little bit

--- app/controllers/studentsearch_controller.rb
+++ app/controllers/studentsearch_controller.rb
@@ -1,59 +1,54 @@
 class StudentsearchController < ApplicationController
     def index
-        result = nil
+        @students = []
         if params.include?(:q) and params[:q] != ''
                 @query = params[:q]
                 param = "%#{@query.downcase}%"
-                result = User.search_student(param)
+                @students = User.search_student(param)
         end
 
         if params.include?("Language")
-            params["Language"].each{
-                |name|
-
-                    if result
-                        result = result & User.search_students_by_language(name)
-                    else
-                        result = User.search_students_by_language(name)
-                    end
-            }
+            @students = @students.concat(find_and_add_users(params["Language"], User.method(:search_students_by_language))).uniq {|s| s.id}
         end
 
         if params.include?("ProgrammingLanguage")
-            params["ProgrammingLanguage"].each{
-                |name|
-                    if result
-                        result = result & User.search_students_by_programming_language(name)
-                    else
-                        result = User.search_students_by_programming_language(name)
-                    end
-            }
+            @students = @students.concat(find_and_add_users(params["ProgrammingLanguage"], User.method(:search_students_by_programming_language))).uniq {|s| s.id}
         end
 
         if params.include?(:semester) and params[:semester] != ''
             @semester = params[:semester]
             tmp = params[:semester]
-                studentForSemester = []
+                student_for_semester = []
                 tmp = tmp.split(',')
-                tmp.each{
+                tmp.each do
                     |str|
                         int = str.to_i
                         if int > 0 and int < 20
                             student_for_semester = student_for_semester + User.where(semester: int)
                         end
-                }
-            if result    
-                result = result & student_for_semester  
-            else
-                result = student_for_semester
-            end
+                end
+
+            
+            @students = @students.concat(student_for_semester).uniq {|s| s.id}
+            
         end
-        if not result
+
+        if @students.length == 0
             @no_search = true
-            @students = []
         else
-            @no_search = false
-            @students = result  
+            @no_search = false  
         end
     end
+
+
+    private
+
+        def find_and_add_users(names, function)
+            res = []
+            names.each do |name|
+                res.concat(function.call(name))
+            end
+
+            return res
+        end
 end
