00a165e10e8ed9a343349ddd38ec9cd54dbd419a
Tim <tim@tim-specht.de>
2014-01-05 16:16:09 +0100

    some more refactoring

--- app/controllers/studentsearch_controller.rb
+++ app/controllers/studentsearch_controller.rb
@@ -7,37 +7,19 @@ class StudentsearchController < ApplicationController
                 @students = User.search_student(param)
         end
 
-        if params.include?("Language")
-            @students = @students.concat(find_and_add_users(params["Language"], User.method(:search_students_by_language))).uniq {|s| s.id}
-        end
-
-        if params.include?("ProgrammingLanguage")
-            @students = @students.concat(find_and_add_users(params["ProgrammingLanguage"], User.method(:search_students_by_programming_language))).uniq {|s| s.id}
-        end
+        @students = @students.concat(find_and_add_users(params["Language"], User.method(:search_students_by_language))).uniq {|s| s.id}
+        @students = @students.concat(find_and_add_users(params["ProgrammingLanguage"], User.method(:search_students_by_programming_language))).uniq {|s| s.id}
+        
 
         if params.include?(:semester) and params[:semester] != ''
             @semester = params[:semester]
-            tmp = params[:semester]
-                student_for_semester = []
-                tmp = tmp.split(',')
-                tmp.each do
-                    |str|
-                        int = str.to_i
-                        if int > 0 and int < 20
-                            student_for_semester = student_for_semester + User.where(semester: int)
-                        end
-                end
 
-            
-            @students = @students.concat(student_for_semester).uniq {|s| s.id}
+            # possibly vulnerable to sql injection!
+            @students = @students.concat(User.where("semester IN (?)", @semester.split(',').map(&:to_i))).uniq {|s| s.id}
             
         end
 
-        if @students.length == 0
-            @no_search = true
-        else
-            @no_search = false  
-        end
+        @no_search = @students.length == 0
     end
 
 
@@ -45,6 +27,10 @@ class StudentsearchController < ApplicationController
 
         def find_and_add_users(names, function)
             res = []
+            if names.nil?
+                return res
+            end
+
             names.each do |name|
                 res.concat(function.call(name))
             end
