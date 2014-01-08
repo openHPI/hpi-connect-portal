4dca0cf618dc8e1a44379c7884adcb1c7f59dc73
Johannes Koch <johannes.koch@student.hpi.uni-potsdam.de>
2014-01-08 23:03:59 +0100

    add form and change mailer for #144

--- app/mailers/applications_mailer.rb
+++ app/mailers/applications_mailer.rb
@@ -8,11 +8,11 @@ class ApplicationsMailer < ActionMailer::Base
   def application_declined_student_email(application)
     send_mail_for_application_to_user(application, (t "applications_mailer.students.declined.subject", job_title: application.job_offer.title, chair: application.job_offer.chair.name))
   end
-  def new_application_notification_email(application)
-    if(!application.user.cv.path.nil?)
+  def new_application_notification_email(application, message, add_cv)
+    if(!application.user.cv.path.nil?) and !add_cv.nil?
       attachments[application.user.cv_file_name] = File.read(application.user.cv.path)
     end
-    send_mail_for_application_to_wimi(application, (t "applications_mailer.wimi.new_application.subject"))
+    send_mail_for_application_to_wimi(application, message, (t "applications_mailer.wimi.new_application.subject"))
   end
 
   private
@@ -20,8 +20,9 @@ class ApplicationsMailer < ActionMailer::Base
         @application = application    
         mail(to: @application.user.email, subject: subject)
     end
-        def send_mail_for_application_to_wimi(application, subject)
+        def send_mail_for_application_to_wimi(application, message, subject)
         @application = application    
+        @message = message
         mail(to: @application.job_offer.responsible_user.email, subject: subject)
     end
 end
