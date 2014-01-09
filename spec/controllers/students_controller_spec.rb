ea4694f2984559491a43163f6793b7d827ab38b2
Frank Blechschmidt <frank.blechschmidt@student.hpi.uni-potsdam.de>
2014-01-09 11:38:52 +0100

    small refactorings in student language and programming language tests

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -224,55 +224,51 @@ describe StudentsController do
   # end
 
   describe "PUT update with programming languages skills" do
-    it "updates the requested student with an existing programming language" do
-      pl = ProgrammingLanguage.create([{name: 'MyProgrammingLanguage'}])
-      pl_id = pl.first.id
-      student = User.create! :firstname => "Test", :lastname => "User", :role => Role.create(:name => "Student"), :semester => "1", :education => "Master", :academic_program => "Volkswirtschaftslehre", :programming_languages => pl, :programming_languages_users => ProgrammingLanguagesUser.create([{programming_language_id: pl_id, skill: '4'}])
+    it "updates the requested research assistant with an existing programming language" do
+      pl = ProgrammingLanguage.create([{name: 'Ruby'}])
+      student = User.create! valid_attributes
+      student.assign_attributes(:programming_languages => pl, :programming_languages_users => ProgrammingLanguagesUser.create([{programming_language_id: pl.first.id, skill: '4'}]))
       ProgrammingLanguagesUser.any_instance.should_receive(:update_attributes).with({ :skill => "2" })
-      put :update, {:id => student.to_param, :user => { "firstname" => "Test2" }, :programming_languages => { pl_id.to_s => "2" }}, valid_session
+      put :update, {:id => student.to_param, :user => { "firstname" => "Max" }, :programming_languages => { pl.first.id.to_s => "2" }}, valid_session
     end
-    it "updates the requested student with a new programming language" do
-      pl = ProgrammingLanguage.create([{name: 'MyProgrammingLanguage'}, {name: 'MySecondProgrammingLanguage'}])
-      pl_id = pl.first.id
-      pl2_id = pl.last.id
-      student = User.create! :firstname => "Test", :lastname => "User", :role => Role.create(:name => "Student"), :semester => "1", :education => "Master", :academic_program => "Volkswirtschaftslehre", :programming_languages => [pl.first], :programming_languages_users => ProgrammingLanguagesUser.create([{programming_language_id: pl_id, skill: '4'}])
-      put :update, {:id => student.to_param, :user => { "firstname" => "Test2" }, :programming_languages => { pl2_id.to_s => "2" }}, valid_session
+    it "updates the requested research assistant with a new programming language" do
+      pl = ProgrammingLanguage.create([{name: 'Ruby'}, {name: 'Go'}])
+      student = User.create! valid_attributes
+      student.assign_attributes(:programming_languages => [pl.first], :programming_languages_users => ProgrammingLanguagesUser.create([{programming_language_id: pl.first.id, skill: '4'}]))
+      put :update, {:id => student.to_param, :user => { "firstname" => "Max" }, :programming_languages => { pl.last.id.to_s => "2" }}, valid_session
       student.programming_languages_users.last.skill == 2
       student.programming_languages.last == pl.last
     end
-    it "updates the requested student with a removed programming language" do
-      pl = ProgrammingLanguage.create([{name: 'MyProgrammingLanguage'}, {name: 'MySecondProgrammingLanguage'}])
-      pl_id = pl.first.id
-      pl2_id = pl.last.id
-      student = User.create! :firstname => "Test", :lastname => "User", :role => Role.create(:name => "Student"), :semester => "1", :education => "Master", :academic_program => "Volkswirtschaftslehre", :programming_languages => pl, :programming_languages_users => ProgrammingLanguagesUser.create([{programming_language_id: pl_id, skill: '4'},{programming_language_id: pl2_id, skill: '2'}])
-      put :update, {:id => student.to_param, :user => { "firstname" => "Test2" }, :programming_languages => { pl_id.to_s => "2" }}, valid_session
+    it "updates the requested research assistant with a removed programming language" do
+      pl = ProgrammingLanguage.create([{name: 'Ruby'}, {name: 'Go'}])
+      student = User.create! valid_attributes
+      student.assign_attributes(:programming_languages => pl, :programming_languages_users => ProgrammingLanguagesUser.create([{programming_language_id: pl.first.id, skill: '4'},{programming_language_id: pl.last.id, skill: '2'}]))
+      put :update, {:id => student.to_param, :user => { "firstname" => "Max" }, :programming_languages => { pl.first.id.to_s => "2" }}, valid_session
       student.programming_languages.size == 1
     end
   end
 
   describe "PUT update with  languages skills" do
-    it "updates the requested student with an existing language" do
-      l = ProgrammingLanguage.create([{name: 'MyLanguage'}])
-      l_id = l.first.id
-      student = User.create! :firstname => "Test", :lastname => "User", :role => Role.create(:name => "Student"), :semester => "1", :education => "Master", :academic_program => "Volkswirtschaftslehre", :programming_languages => l, :languages_users => LanguagesUser.create([{language_id: l_id, skill: '4'}])
+    it "updates the requested research assistant with an existing language" do
+      l = Language.create([{name: 'English'}])
+      student = User.create! valid_attributes
+      student.assign_attributes(:languages => l, :languages_users => LanguagesUser.create([{language_id: l.first.id, skill: '4'}]))
       LanguagesUser.any_instance.should_receive(:update_attributes).with({ :skill => "2" })
-      put :update, {:id => student.to_param, :user => { "firstname" => "Test2" }, :languages => { l_id.to_s => "2" }}, valid_session
+      put :update, {:id => student.to_param, :user => { "firstname" => "Max" }, :languages => { l.first.id.to_s => "2" }}, valid_session
     end
-    it "updates the requested student with a new language" do
-      l = Language.create([{name: 'MyLanguage'}, {name: 'MySecondLanguage'}])
-      l_id = l.first.id
-      l2_id = l.last.id
-      student = User.create! :firstname => "Test", :lastname => "User", :role => Role.create(:name => "Student"), :semester => "1", :education => "Master", :academic_program => "Volkswirtschaftslehre", :languages => [l.first], :languages_users => LanguagesUser.create([{language_id: l_id, skill: '4'}])
-      put :update, {:id => student.to_param, :user => { "firstname" => "Test2" }, :languages => { l2_id.to_s => "2" }}, valid_session
+    it "updates the requested research assistant with a new language" do
+      l = Language.create([{name: 'English'}, {name: 'German'}])
+      student = User.create! valid_attributes
+      student.assign_attributes(:languages => [l.first], :languages_users => LanguagesUser.create([{language_id: l.first.id, skill: '4'}]))
+      put :update, {:id => student.to_param, :user => { "firstname" => "Max" }, :languages => { l.last.id.to_s => "2" }}, valid_session
       student.languages_users.last.skill == 2
       student.languages.last == l.last
     end
-    it "updates the requested student with a removed language" do
-      l = Language.create([{name: 'MyLanguage'}, {name: 'MySecondLanguage'}])
-      l_id = l.first.id
-      l2_id = l.last.id
-      student = User.create! :firstname => "Test", :lastname => "User", :role => Role.create(:name => "Student"), :semester => "1", :education => "Master", :academic_program => "Volkswirtschaftslehre", :languages => l, :languages_users => LanguagesUser.create([{language_id: l_id, skill: '4'},{language_id: l2_id, skill: '2'}])
-      put :update, {:id => student.to_param, :user => { "firstname" => "Test2" }, :languages => { l_id.to_s => "2" }}, valid_session
+    it "updates the requested research assistant with a removed language" do
+      l = Language.create([{name: 'English'}, {name: 'German'}])
+      student = User.create! valid_attributes
+      student.assign_attributes(:languages => l, :languages_users => LanguagesUser.create([{language_id: l.first.id, skill: '4'},{language_id: l.last.id, skill: '2'}]))
+      put :update, {:id => student.to_param, :user => { "firstname" => "Max" }, :languages => { l.first.id.to_s => "2" }}, valid_session
       student.languages.size == 1
     end
   end
