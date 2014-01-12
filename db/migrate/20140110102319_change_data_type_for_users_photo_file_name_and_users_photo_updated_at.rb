22b5d2bc516be9f206d26577a24f5a69e12c1d92
Judith Hartmann <judith.hartmann@student.hpi.uni-potsdam.de>
2014-01-12 15:14:47 +0100

    Merge remote-tracking branch 'origin/develop' into develop-ja
    Conflicts:
    	config/locales/views/students/de.yml
    	config/locales/views/students/en.yml

---
+++ db/migrate/20140110102319_change_data_type_for_users_photo_file_name_and_users_photo_updated_at.rb
@@ -0,0 +1,17 @@
+class ChangeDataTypeForUsersPhotoFileNameAndUsersPhotoUpdatedAt < ActiveRecord::Migration
+  def change
+  	change_column :users, :photo_file_name, :string
+  	change_column :users, :photo_updated_at, :timestamp
+  end
+
+  def self.up
+    change_table :users do |t|
+      t.attachment :photo
+    end
+  end
+
+  def self.down
+    drop_attached_file :users, :photo
+  end
+
+end
