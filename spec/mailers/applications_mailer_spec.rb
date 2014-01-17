192c5265229f5fb4403f85cddb712decbaebeb7c
Johannes Koch <johannes.koch@student.hpi.uni-potsdam.de>
2014-01-17 15:03:42 +0100

    fixed test problems regarding to the multi upload feature

--- spec/mailers/applications_mailer_spec.rb
+++ spec/mailers/applications_mailer_spec.rb
@@ -1,5 +1,5 @@
 
-require "spec_helper"
+require "email_spec_helper"
 
 describe ApplicationsMailer do
 	include EmailSpec::Helpers
@@ -64,12 +64,15 @@ describe ApplicationsMailer do
 		before(:each) do			
 		  @test_file = ActionDispatch::Http::UploadedFile.new({
 		    :filename => 'test_picture.jpg',
-		    :type => 'image/jpg',
+		    :type => 'image/jpeg',
 		    :tempfile => fixture_file_upload('/images/test_picture.jpg')
 		  })
 
 		  @message = "Testmessage"
-			@email = ApplicationsMailer.new_application_notification_email(@application, @message, true).deliver
+			@email = ApplicationsMailer.new_application_notification_email(@application, @message, false, {:file_attributes => [:file => @test_file] }).deliver
+			
+			html = get_message_part(@email, /html/)
+			@html_body = html.body.raw_source
 		end
 		it "should send an email" do
 			ActionMailer::Base.deliveries.count.should == 1
@@ -84,22 +87,26 @@ describe ApplicationsMailer do
 		end
 
 		it "should have the title of the joboffer in the body" do
-			@email.body.should have_content(@job_offer.title)
+			@html_body.should have_content(@job_offer.title)
 		end
 		it "should have the name of the student in the body" do
-			@email.body.should have_content(@student.firstname)
-			@email.body.should have_content(@student.lastname)
+			@html_body.should have_content(@student.firstname)
+			@html_body.should have_content(@student.lastname)
 		end
 		it "should include the link to the students profile page" do
 			@email.should have_body_text(url_for(controller:"students", action: "show", id: @student.id, only_path: false))
 		end
 		it "should have the personal application message in the body" do
-			@email.body.should have_content(@message)
+			@html_body.should have_content(@message)
 		end
 		it "should include the students cv if activated" 
 
 		it "should include all attached files the student chose" do
-			#@email.attachments.should include(@test_file)
+			@email.attachments.should have(1).attachment
+			attachment = @email.attachments[0]
+			attachment.should be_a_kind_of(Mail::Part)
+			attachment.content_type.should be_start_with(@test_file.content_type)
+			attachment.filename.should == @test_file.original_filename
 		end
 	end
 end
