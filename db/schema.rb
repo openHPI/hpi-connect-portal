d9f061c940c6f1ec0152ba5f87e13c3be8679627
Daniel Neuschäfer-Rube <daniel.neuschaefer@gmail.com>
2014-01-27 11:58:08 +0100

    - added translation for FAQs #117   * FAQs now have a "locale" attribute, added migration   * only FAQs for the current locale are shown   * FAQs are created with the current locale   * added translation for current FAQs to German

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140117180848) do
+ActiveRecord::Schema.define(version: 20140127093416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20140117180848) do
     t.text     "answer"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "locale"
   end
 
   create_table "job_offers", force: true do |t|
