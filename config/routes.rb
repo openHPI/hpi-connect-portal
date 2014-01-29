d7d166ef2dad8a4c8208e98a66e2e8116b5cd0c5
jummethum <janek.ummethum@student.hpi.uni-potsdam.de>
2014-01-29 20:55:20 +0100

    Removed duplicate entries from the routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -2,70 +2,67 @@ HpiHiwiPortal::Application.routes.draw do
   mount Bootsy::Engine => '/bootsy', as: 'bootsy'
   scope "(:locale)", locale: /en|de/ do
 
-    get "imprint/index"
-    get "get-userlist" => "users#userlist"
+  get "imprint/index"
+  
+  root :to => "login_page#index"
 
-    resources :user_statuses
-      root :to => "login_page#index"
-
-    resources :job_offers do
-      collection do
-        get "archive"
-        get "matching"
-      end
-      member do
-        get "complete"
-        get "accept"
-        get "decline"
-        get "reopen"
-        put "prolong"
-      end
+  resources :job_offers do
+    collection do
+      get "archive"
+      get "matching"
     end
-
-    get "employers/external", to: "employers#index_external", as: "external_employers"
-    
-    resources :employers do
-        collection do 
-            post 'update_staff'
-        end
+    member do
+      get "complete"
+      get "accept"
+      get "decline"
+      get "reopen"
+      put "prolong"
     end
+  end
 
-    resources :applications, only: [:create, :destroy] do
-      member do
-        get "accept"
-        get "decline"
-      end
+  get "get-userlist" => "users#userlist"
+  get "employers/external", to: "employers#index_external", as: "external_employers"
+
+  resources :employers do
+    collection do 
+        post 'update_staff'
     end
+  end
 
-    resources :users, only: [:edit, :update]
+  #resources :users, only: [:edit, :update]
 
-    resources :faqs
-
-    resources :staff, except: [:new, :create] do
-        collection do
-            post 'update_role'
-            post 'set_role_to_student'
-        end
+  resources :applications, only: [:create, :destroy] do
+    member do
+      get "accept"
+      get "decline"
     end
+  end
+
+  devise_for :users, controllers: { sessions: 'sessions' }
+
+  resources :programming_languages
+  resources :languages
 
   resources :users, only: [:show, :edit, :update]
   resources :user_statuses
 
-	resources :students do
-        collection do
-            get 'students/new' => 'students#new'
-            post 'students' => 'students#create'
-            get 'matching'
-            post 'update_role'
-        end
+  resources :studentsearch
+  resources :faqs
+
+  resources :staff, except: [:new, :create] do
+      collection do
+          post 'update_role'
+          post 'set_role_to_student'
+      end
+  end
+
+  resources :students do
+    collection do
+      get 'students/new' => 'students#new'
+      post 'students' => 'students#create'
+      get 'matching'
+      post 'update_role'
     end
-
-    devise_for :users, controllers: { sessions: 'sessions' }
-
-    resources :programming_languages
-    resources :languages
-
-    resources :users
-    resources :user_statuses
+  end
   end
 end
