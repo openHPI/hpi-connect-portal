f00d5a11c7c57944b721f640c5518edf29adf700
Daniel Neuschäfer-Rube <daniel.neuschaefer@gmail.com>
2014-01-31 14:23:16 +0100

    - added "sanitize" gem - implemented "sanitize_html" to strip all html from a string except stuff from a whitelist --> this allows us to still use bootsy but work around the security risks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
   def mark_if_required(object, attribute)
     "*" if object.class.validators_on(attribute).map(&:class).include? ActiveRecord::Validations::PresenceValidator  
   end 
+
+  def sanitize_html(html)
+    Sanitize.clean(html, Sanitize::Config::BASIC).html_safe
+  end
 end
