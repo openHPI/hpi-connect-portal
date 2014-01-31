981b5c3af9f393cbfb434aab2dd70d2213bd0eb5
Tim <tim@tim-specht.de>
2014-01-31 10:08:54 +0100

    refactored job_offer model

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -21,6 +21,7 @@
 
 class JobOffer < ActiveRecord::Base
   include Bootsy::Container
+  include JobOfferScopes
 
   before_save :default_values
 
@@ -43,21 +44,6 @@ class JobOffer < ActiveRecord::Base
   validates_datetime :end_date, on_or_after: :start_date, allow_blank: :end_date
 
   self.per_page = 5
-
-  scope :pending, -> { where(status_id: JobStatus.pending.id) }
-  scope :open, -> { where(status_id: JobStatus.open.id) }
-  scope :running, -> { where(status_id: JobStatus.running.id) }
-  scope :completed, -> { where(status_id: JobStatus.completed.id) }
-
-  scope :filter_employer, -> employer { where(employer_id: employer) }
-  scope :filter_start_date, -> start_date { where('start_date >= ?', Date.parse(start_date)) }
-  scope :filter_end_date, -> end_date { where('end_date <= ?', Date.parse(end_date)) }
-  scope :filter_time_effort, -> time_effort { where('time_effort <= ?', time_effort.to_f) }
-  scope :filter_compensation, -> compensation { where('compensation >= ?', compensation.to_f) }
-  scope :filter_programming_languages, -> programming_language_ids { joins(:programming_languages).where('programming_languages.id IN (?)', programming_language_ids).uniq}
-  scope :filter_languages, -> language_ids { joins(:languages).where('languages.id IN (?)', language_ids).uniq}
-  scope :filter_external_employer_only, -> external_only { joins(:employer).where('employers.external = ?', true) }
-  scope :search, -> search_string { includes(:programming_languages, :employer).where('lower(title) LIKE ? OR lower(job_offers.description) LIKE ? OR lower(employers.name) LIKE ? OR lower(programming_languages.name) LIKE ?', "%#{search_string}%".downcase, "%#{search_string}%".downcase, "%#{search_string}%".downcase, "%#{search_string}%".downcase).references(:programming_languages,:employer) }
 
   def default_values
     self.status ||= JobStatus.pending
@@ -93,11 +79,7 @@ class JobOffer < ActiveRecord::Base
   end
 
   def human_readable_compensation
-    if self.compensation == 10.0
-      I18n.t('job_offers.default_compensation')
-    else
-      self.compensation.to_s + " " + I18n.t("job_offers.compensation_description")
-    end
+    (self.compensation == 10.0) ? I18n.t('job_offers.default_compensation') : self.compensation.to_s + " " + I18n.t("job_offers.compensation_description")
   end
 
   def check_remaining_applications
