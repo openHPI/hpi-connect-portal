509735f27f784bf9bfe42e427c2c9e7c47dd2a4a
Daniel Neuschäfer-Rube <daniel.neuschaefer@gmail.com>
2014-01-31 15:13:49 +0100

    - added test for "sanitize_html" #293 - adjusted standard buttons for bootsy, removing the non-functional image button and adding the view-as-html button

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  describe "sanitize_html" do
+    it "removes tags like <script> from html strings" do    
+      script_string = "<script> alert('I am a malicious script.'); </script>"
+      sanitize_html(script_string).should_not include "<script>"
+    end
+  end  
+end
