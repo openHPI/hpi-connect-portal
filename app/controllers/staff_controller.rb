924ae7beaa4bb392161b68c27ec21c40158cc26b
Tim <tim@tim-specht.de>
2014-02-01 12:06:32 +0100

    removed unused before filters

--- app/controllers/staff_controller.rb
+++ app/controllers/staff_controller.rb
@@ -2,8 +2,6 @@ class StaffController < ApplicationController
   include UsersHelper
 
   authorize_resource class: "User"
-  #before_filter :check_user_can_index_staff, only: [:index]
-  #before_filter :check_current_user_or_admin, only: [:edit]
 
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
@@ -74,18 +72,5 @@ class StaffController < ApplicationController
         :birthday, :additional_information, :homepage,
         :github, :facebook, :xing, :photo, :cv, :linkedin, :user_status_id,
         :language_ids => [],:programming_language_ids => [])
-    end
-
-    def check_current_user_or_admin
-      set_user
-      unless current_user? @user or user_is_admin?
-        redirect_to staff_path @user
-      end
-    end
-
-    def check_user_can_index_staff
-      unless user_is_admin?
-        redirect_to root_path
-      end
     end
 end
