33f40f93717406a275fdb0ef94e3f28342c56f26
steier <jksteier@gmail.com>
2014-02-02 21:28:36 +0100

    some acceptance tests

--- spec/features/staff_spec.rb
+++ spec/features/staff_spec.rb
@@ -8,36 +8,58 @@ describe "the staff page" do
 	before(:each) do
     FactoryGirl.create(:role, :student)
     @staff1 = FactoryGirl.create(:user, :staff)
+    @programming_language = FactoryGirl.create(:programming_language)
+    @student1 = FactoryGirl.create(:user, :student, :programming_languages =>[@programming_language])
+    FactoryGirl.create(:role, :student)
     login_as(admin, :scope => :user)
     visit staff_index_path
 
   end
 
-  it "should view only names of a staff member on the overview" do
-    page.should have_content(
-      @staff1.firstname,
-      @staff1.lastname,
-    )
+  describe "as an admin" do
+    it "should view only names of a staff member on the overview" do
+      page.should have_content(
+        @staff1.firstname,
+        @staff1.lastname,
+      )
+    end
+
+    it "should contain a link for showing a profile and it should lead to profile page " do
+      find_link(@staff1.firstname).click
+
+      current_path.should_not == staff_index_path
+      current_path.should == staff_path(@staff1)
+    end
+
+    it "should have a button to demote a member of the staff " do
+      #should have_button('Demote')
+    end
+  end
+  
+
+  describe "as an member of staff" do
+
+    it "should not be visible " do
+      FactoryGirl.create(:job_status, name: 'open')
+      login_as(@staff1, :scope => :user)
+      visit staff_index_path
+      current_path.should_not == staff_index_path
+      current_path.should == job_offers_path
+    end
   end
 
-  it "should contain a link for showing a profile and it should lead to profile page " do
-    find_link(@staff1.firstname).click
+   describe "as an member of staff" do
 
-    current_path.should_not == staff_index_path
-    current_path.should == staff_path(@staff1)
+    it "should not be visible " do
+      FactoryGirl.create(:job_status, name: 'open')
+      login_as(@student1, :scope => :user)
+      visit staff_index_path
+      current_path.should_not == staff_index_path
+      current_path.should == job_offers_path
+    end
+
   end
-
-  # it "should have a button to promoto a member of the staff " do
-  #   should have_button('Promote')
-  # end
-
-  it "should not be visible for a member of the staff " do
-    FactoryGirl.create(:job_status, name: 'open')
-    login_as(@staff1, :scope => :user)
-    visit staff_index_path
-    current_path.should_not == staff_index_path
-    current_path.should == job_offers_path
-  end
+end
 
 describe "the staffs profile page" do
 
@@ -45,24 +67,45 @@ describe "the staffs profile page" do
 
   before(:each) do
     @staff1 = FactoryGirl.create(:user, :role => staff_role, employer: FactoryGirl.create(:employer))
-
     @staff2 = FactoryGirl.create(:user, :role => staff_role, employer: FactoryGirl.create(:employer))
   end
 
-  it "should have an edit link on the show page of the own profile which leads to the staffs edit page" do
+  describe "as a staff member" do
+
+    before(:each) do
       login_as(@staff1, :scope => :user)
-      visit staff_path(@staff1)
-      page.find_link('Edit').click
-      page.current_path.should == edit_staff_path(@staff1)
+    end
+
+    it "should have an edit link on the show page of the own profile which leads to the staffs edit page" do
+        visit staff_path(@staff1)
+        page.find_link('Edit').click
+        page.current_path.should == edit_staff_path(@staff1)
+    end
+
+    it "should not have an edit link on the show page of someone elses profile" do
+        visit staff_path(@staff2)
+        should_not have_link('Edit')
+        visit edit_staff_path(@staff2)
+        current_path.should ==  staff_path(@staff2)
+    end
   end
 
-  it "should not have an edit link on the show page of someone elses profile" do
-      login_as(@staff1, :scope => :user)
-      visit staff_path(@staff2)
-      should_not have_link('Edit')
-      visit edit_staff_path(@staff2)
-      current_path.should ==  staff_path(@staff2)
+  describe "as an admin" do
+
+    it "should be editable" do
+        login_as(@staff1, :scope => :user)
+        visit staff_path(@staff1)
+        page.find_link('Edit').click
+        page.current_path.should == edit_staff_path(@staff1)
+    end
+
+    # it "should have a demote button on listing" do
+    #   page.find_link('Demote')
+    # end
+
   end
 
+
 end
-end
+
+
