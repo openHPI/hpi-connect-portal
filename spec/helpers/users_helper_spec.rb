6459e9bdf897462f6c06d08196d10f0fb59ef8b8
Tim <tim@tim-specht.de>
2014-02-03 12:28:51 +0100

    added some usershelper tests

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,15 +1,30 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the UsersHelper. For example:
-#
-# describe UsersHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe UsersHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    helper.stub(:signed_in?) { true }
+  end
+
+  it "check if user is staff of employer" do
+    job_offer = FactoryGirl.create(:job_offer)
+
+    helper.stub(:current_user) { FactoryGirl.create(:user, :staff, employer: job_offer.employer) }
+    assert helper.user_is_staff_of_employer?(job_offer)
+
+    helper.stub(:current_user) { FactoryGirl.create(:user, :student, employer: job_offer.employer) }
+    assert !helper.user_is_staff_of_employer?(job_offer)
+
+    helper.stub(:current_user) { FactoryGirl.create(:user, :staff, employer: FactoryGirl.create(:employer)) }
+    assert !helper.user_is_staff_of_employer?(job_offer)
+  end
+
+  it "checks if the user is the responsible user" do
+    job_offer = FactoryGirl.create(:job_offer, responsible_user: FactoryGirl.create(:user, :staff))
+
+    helper.stub(:current_user) { job_offer.responsible_user }
+    assert helper.user_is_responsible_user?(job_offer)
+
+    helper.stub(:current_user) { FactoryGirl.create(:user, :staff) }
+    assert !helper.user_is_responsible_user?(job_offer)
+  end
 end
