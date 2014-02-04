534f6a022bdf088dae23f31d19b05082f900bd97
alexanderernst <leverkusener77@googlemail.com>
2014-02-04 16:45:37 +0100

    #162 changed userlist method to only return staff members of the users employer except the deputy

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,7 @@ describe UsersController do
   let(:student) { FactoryGirl.create(:user, :student) }
   let(:staff) { FactoryGirl.create(:user, :staff) }
   let(:admin) { FactoryGirl.create(:user, :admin) }
+  let(:employer) { FactoryGirl.create(:employer)}
   let(:default_params) { {format: :json, exclude_user: admin.to_param} }
 
   let(:valid_session) { {} }
@@ -58,14 +59,44 @@ describe UsersController do
 
   describe "GET userlist" do
 
-    it "renders json" do
+    before do
       sign_in admin
+    end
+
+    it "should not return users when user is admin" do
       second_student = FactoryGirl.create :user, :student
       get :userlist, default_params
       parsed_response = JSON.parse response.body 
       parsed_response["is_deputy"].should == false
-      parsed_response["users"].size.should == 1
+      parsed_response["users"].should == nil
     end
+
+    it "should not return users when current_user is not allowed" do
+      sign_in student
+      employer = FactoryGirl.create(:employer)
+      staff.update(employer: employer)
+      FactoryGirl.create(:user, :staff, employer: employer)
+      get :userlist, {format: :json, exclude_user: staff.to_param}
+      parsed_response = JSON.parse response.body 
+      parsed_response["is_deputy"].should == false
+      parsed_response["users"].should == nil
+    end
+
+    it "only returns list of all students and staff of this employer except the user itself" do
+      a_student = FactoryGirl.create :user, :student
+      staff_member = FactoryGirl.create :user, :staff, employer: employer
+      deputy = FactoryGirl.create :user, :staff, employer: employer
+      employer.update deputy: deputy
+      FactoryGirl.create :user, :staff, employer: FactoryGirl.create(:employer)
+
+      get :userlist, {format: :json, exclude_user: deputy.to_param}
+      parsed_response = JSON.parse response.body 
+      parsed_response["is_deputy"].should == true
+      expect(parsed_response["users"].size).to equal(2)
+      expect(parsed_response["users"][0]["full_name"]).to eq a_student.full_name
+      expect(parsed_response["users"][1]["full_name"]).to eq staff_member.full_name
+    end
+
   end
 
 end
