901ff9e2e95124108e7d48f745be81f32567a577
ephe-meral <j.appel.online@gmail.com>
2014-02-07 15:04:55 +0100

    enhanced promote/demote spec stub

--- spec/features/promote_demote_spec.rb
+++ spec/features/promote_demote_spec.rb
@@ -9,7 +9,6 @@ describe "the students listing" do
   subject { page }
 
   let(:employer) { FactoryGirl.create(:employer) }
-  let(:staff)    { FactoryGirl.create(:user, role: FactoryGirl.create(:role, :staff), employer: employer) }
   let(:deputy)   { employer.deputy }
 
   before(:each) do
@@ -18,12 +17,41 @@ describe "the students listing" do
   end
 
   it "should have promote button for deputies" do
-    # staff creates a new job offer for his employer
     login_as(deputy, :scope => :user)
 
     visit students_path
     expect(page).to have_content(@student1.firstname, @student1.lastname)
 
     page.should have_button("Promote")
+  end
+end
+
+describe "the chair page" do
+  let(:employer) { FactoryGirl.create(:employer) }
+  let(:deputy)   { employer.deputy }
+
+  before(:each) do
+    @staff1 = FactoryGirl.create(:user, role: FactoryGirl.create(:role, :staff), employer: employer)
+    @job_offer_open = FactoryGirl.create(:job_offer, employer: employer, status: FactoryGirl.create(:job_status, :open))
+    @job_offer_running = FactoryGirl.create(:job_offer, employer: employer, status: FactoryGirl.create(:job_status, :running))
+    @job_offer_pending = FactoryGirl.create(:job_offer, employer: employer, status: FactoryGirl.create(:job_status, :pending))
+  end
+
+  it "should have promote buttons next to staff for deputies" do
+    login_as(deputy, :scope => :user)
+
+    visit employer_path(employer)
+    expect(page).to have_content(@staff1.firstname, @staff1.lastname)
+
+    page.should have_button("Promote")
+  end
+
+  it "should have demote buttons next to staff for deputies" do
+    login_as(deputy, :scope => :user)
+
+    visit employer_path(employer)
+    expect(page).to have_content(@staff1.firstname, @staff1.lastname)
+
+    page.should have_button("Demote")
   end
 end
