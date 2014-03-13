8a00a3c7bb06e2d90fe3eaa539e21d4a6d0ce32e
alexanderernst <leverkusener77@googlemail.com>
2014-03-13 15:58:51 +0100

    #326 hack for navbar implemented

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -1,3 +1,13 @@
   $(function() {
-    
+    $("#csshack_header_image").width($("body").width()*3/4-20);
+    $("div.container").first().css({
+    "margin-left": ($("body").width()/8-10) + "px"
+    });
+    $("div.bigheader").css({
+        "margin-left": ($("body").width()/8) + "px"
+    });
+    $("div.smallheader.headwrapper").css({
+      width: $("body").width()*3/4 +"px",
+      height: "148px"
+    });
 })
