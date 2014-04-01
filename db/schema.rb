a7dd08f5824e171b082ea3cb53c2fea3a29b080d
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-04-01 14:58:26 +0200

    solved migration issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401121723) do
+ActiveRecord::Schema.define(version: 20140328192025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,7 +105,7 @@ ActiveRecord::Schema.define(version: 20140401121723) do
     t.integer  "status_id",           default: 1
     t.integer  "vacant_posts"
     t.boolean  "flexible_start_date", default: false
-    t.integer  "category_id"
+    t.integer  "category_id",         default: 0,     null: false
   end
 
   create_table "job_offers_languages", id: false, force: true do |t|
