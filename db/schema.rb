1e6ba1766a9a3002da4138fb3a03de58514ab848
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-04-02 16:00:24 +0200

    Added missing modified schema #329

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140328192025) do
+ActiveRecord::Schema.define(version: 20140402125519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 20140328192025) do
     t.integer  "vacant_posts"
     t.boolean  "flexible_start_date", default: false
     t.integer  "category_id",         default: 0,     null: false
+    t.integer  "state_id",            default: 3,     null: false
   end
 
   create_table "job_offers_languages", id: false, force: true do |t|
