e6f87d30702b0188ddca5d1c763ce6256efa2366
alexanderernst <leverkusener77@googlemail.com>
2014-04-05 12:08:06 +0200

    created new homepage added 5 latest job offers and 3 latest employers deleted sessioncontroller #326

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,30 @@
 class HomeController < ApplicationController
   skip_before_filter :signed_in_user
   skip_before_filter :verify_authenticity_token
+  skip_before_filter :signed_in_user
 
   def index
+    @job_offers = JobOffer.last 5
+    @employers = Employer.last 3
   end
 
   def imprint
   end
+
+  def create
+    user = User.find_by_email params[:session][:email]
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or root_path
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'index'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+
 end
---
+++ spec/features/home_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "the home page" do 
+
+  it "renders the latest 5 job offers" do
+    6.times { |i| FactoryGirl.create(:job_offer, title:"Testjob#{i+1}") }
+    visit root_path
+    page.should have_content(
+      "Testjob2", "Testjob3", "Testjob4", "Testjob5", "Testjob6"
+    )
+    page.should_not have_content "Testjob1"
+  end
+
+  it "renders login for not signed in users" do
+    visit root_path
+    page.should have_content "Login"
+  end
+
+  it "renders login not for signed in users" do
+    student = FactoryGirl.create(:student)
+    login student.user
+    visit root_path
+    page.should_not have_content "Login"
+  end
+
+  it "renders the latest 3 employers" do
+    4.times { |i| FactoryGirl.create(:employer, name:"TestEmployer#{i+1}") }
+    visit root_path
+    page.should have_content(
+      "TestEmployer2", "TestEmployer3", "TestEmployer4"
+    )
+    page.should_not have_content "TestEmployer1"
+  end
+end
