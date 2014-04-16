46b08ea82bc4c1e7ce3487b3aafa47f0588de578
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-04-16 13:19:01 +0200

    Added academic_program to Job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140408141025) do
+ActiveRecord::Schema.define(version: 20140412203413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20140408141025) do
     t.integer  "category_id",         default: 0,     null: false
     t.integer  "state_id",            default: 3,     null: false
     t.integer  "graduation_id",       default: 2,     null: false
+    t.integer  "academic_program_id"
   end
 
   create_table "job_offers_languages", id: false, force: true do |t|
