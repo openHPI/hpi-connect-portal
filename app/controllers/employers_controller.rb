72dafcf34147696d9cc6b1daaac88d60f5b9241c
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-04-26 20:12:33 +0200

    fixed commit again #344

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -7,7 +7,8 @@ class EmployersController < ApplicationController
   def index
     @employers = can?(:activate, Employer) ? Employer.all : Employer.active
     @employers = @employers.sort_by { |employer| employer.name }
-    @employers = @employers.paginate page: params[:page], per_page: 15class EmployersController < ApplicationController
+    @employers = @employers.paginate page: params[:page], per_page: 15
+  end
 
   def show
     not_found unless @employer.activated || can?(:activate, @employer) || !current_user || (current_user && (current_user.staff? && current_user.manifestation.employer == @employer)
@@ -71,18 +72,3 @@ class EmployersController < ApplicationController
 end
 
   end
-
-  private
-
-    def rescue_from_exception(exception)
-      redirect_to employers_path, notice: exception.message
-    end
-
-    def set_employer
-      @employer = Employer.find params[:id]
-    end
-
-    def employer_params
-      params.require(:employer).permit(:name, :description, :avatar, :head, :deputy_id, deputy_attributes: [ user_attributes: [:firstname, :lastname, :email, :password, :password_confirmation ]])
-    end
-end
