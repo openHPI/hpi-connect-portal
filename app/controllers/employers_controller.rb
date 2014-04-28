8e25107504deef1fd1b58ebc3a4f769d30d046af
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-04-28 10:10:54 +0200

    employer index and show visible #344

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,6 +1,6 @@
 class EmployersController < ApplicationController
 
-  skip_before_filter :signed_in_user, only: [:show, :new, :create]
+  skip_before_filter :signed_in_user, only: [:index, :show, :new, :create]
 
   authorize_resource only: [:edit, :update, :activate]
   before_action :set_employer, only: [:show, :edit, :update, :activate]
@@ -12,7 +12,7 @@ class EmployersController < ApplicationController
   end
 
   def show
-    not_found unless @employer.activated || can?(:activate, @employer) || !current_user || (current_user && (current_user.staff? && current_user.manifestation.employer == @employer))
+    not_found unless @employer.activated || can?(:activate, @employer) || !current.user || (current_user && (current_user.staff? && current_user.manifestation.employer == @employer))
     page = params[:page]
     @staff =  @employer.staff_members.where.not(id: @employer.deputy.id).paginate page: page
     @running_job_offers = @employer.job_offers.running.paginate page: page
