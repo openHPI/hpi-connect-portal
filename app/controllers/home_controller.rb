060dc76dff0554312e3cfdbba693a9c90ec5c8df
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-05-12 13:46:08 +0200

    solved #399

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,4 +9,22 @@ class HomeController < ApplicationController
 
   def imprint
   end
+
+  def create
+    user = User.find_by_email params[:session][:email]
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or root_path
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      @job_offers = JobOffer.last 5
+      @employers = Employer.last 3
+      render 'index'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
 end
