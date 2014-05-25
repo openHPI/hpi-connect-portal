48721e8b42f1f09fbfbdac75606284ab0ce0c8fa
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-05-25 02:42:08 +0200

    improvements

--- app/mailers/applications_mailer.rb
+++ app/mailers/applications_mailer.rb
@@ -36,7 +36,7 @@ class ApplicationsMailer < ActionMailer::Base
       @application = application
       @message = message      
       @application.job_offer.employer.staff_members.each { |staff| 
-        @staff_name = staff
+        @staff = staff
         mail(to: staff.email, subject: subject).deliver }
     end
 end
