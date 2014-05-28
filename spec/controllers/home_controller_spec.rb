d735c3eaf196ae26f6f9545174bc859c29ad655f
alexanderernst <leverkusener77@googlemail.com>
2014-05-28 07:48:34 +0200

    now only active JobOffers and Employers on Homepage

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe HomeController do
 
+  before(:all) do
+    FactoryGirl.create(:job_status, :pending)
+    FactoryGirl.create(:job_status, :active)
+    FactoryGirl.create(:job_status, :closed)
+  end
+
   before(:each) do
     login FactoryGirl.create(:student).user
   end
@@ -13,10 +19,17 @@ describe HomeController do
     end
 
     it "assigns latest 5 job_offers" do
-      6.times { |i| FactoryGirl.create(:job_offer, title:"Testjob#{i+1}") }
+      6.times { |i| FactoryGirl.create(:job_offer, title:"Testjob#{i+1}", status: JobStatus.active) }
       get 'index'
       assigns(:job_offers).length.should eq(5)
       assigns(:job_offers).should_not include(JobOffer.find_by_title("Testjob1"))
+    end
+
+    it "does not assign pending JobOffers" do
+      5.times { |i| FactoryGirl.create(:job_offer, title:"Testjob#{i+1}", status: JobStatus.active) }
+      pending_job = FactoryGirl.create(:job_offer, title: "Pending Job", status: JobStatus.pending)
+      get 'index'
+      assigns(:job_offers).should_not include(pending_job)
     end
 
     it "assigns latest 3 employers" do
@@ -25,6 +38,14 @@ describe HomeController do
       assigns(:employers).length.should eq(3)
       assigns(:employers).should_not include(JobOffer.find_by_title("TestEmployer1"))
     end
+
+    it "does not assign not activated employers" do
+      3.times { |i| FactoryGirl.create(:employer, name:"TestEmployer#{i+1}") }
+      not_activated_employer = FactoryGirl.create(:employer, activated: false, name:"NotActiveEmployer")
+      get 'index'
+      assigns(:employers).should_not include not_activated_employer
+    end
+
   end
 
   describe "GET 'imprint'" do
