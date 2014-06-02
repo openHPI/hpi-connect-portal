41d9f6041091aff7d94d6713cd0f32d01062b71c
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-06-02 17:05:52 +0200

    added modal

--- app/assets/javascripts/job_offers.js
+++ app/assets/javascripts/job_offers.js
@@ -1,5 +1,24 @@
 $(document).ready(function($) {
+	$('select').change(function(){
+       //var selected = $(this).find('option:selected');
+       //alert(extra);
+       //alert(selected);
+       //hasAttribute
+
+       	var category = document.getElementById('category_id')
+       	var selected_category = category.options[category.selectedIndex]
+       	var can_book = selected_category.getAttribute('can_book')=="true" ? true : false
+    	if(!can_book)
+    	{
+       		$('#book_modal').modal
+       		$('#book_modal').modal('show')
+		}       	
+    });
       $(".clickable").click(function() {
             window.document.location = $(this).attr("href");
       });
 });
+	function CheckForAllowedCategory(category)
+	{
+		//alert(category);
+	};
