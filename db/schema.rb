8898c9ca1bf4ccda1ea0703b2dc4a32922175769
Daniel Dummer <daniel.dummer@student.hpi.uni-potsdam.de>
2014-06-18 12:37:18 +0200

    removed academic program from job offer controller, factory and specs #439_remove_program_and_room_number

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618095929) do
+ActiveRecord::Schema.define(version: 20140618102325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,7 +133,6 @@ ActiveRecord::Schema.define(version: 20140618095929) do
     t.integer  "category_id",         default: 0,     null: false
     t.integer  "state_id",            default: 3,     null: false
     t.integer  "graduation_id",       default: 2,     null: false
-    t.integer  "academic_program_id"
     t.boolean  "prolong_requested",   default: false
     t.boolean  "prolonged",           default: false
     t.datetime "prolonged_at"
--- spec/factories/job_offer_factory.rb
+++ spec/factories/job_offer_factory.rb
@@ -17,7 +17,6 @@
 #  category_id         :integer          default(0), not null
 #  state_id            :integer          default(3), not null
 #  graduation_id       :integer          default(2), not null
-#  academic_program_id :integer
 #  prolong_requested   :boolean          default(FALSE)
 #  prolonged           :boolean          default(FALSE)
 #  prolonged_at        :datetime
