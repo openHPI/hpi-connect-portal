c83df1b5d97d7e324b40208d248e6605066ad154
Daniel Dummer <daniel.dummer@student.hpi.uni-potsdam.de>
2014-06-18 12:36:38 +0200

    removed academic program id from job offer model #439_remove_program_and_room_number

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -17,7 +17,6 @@
 #  category_id         :integer          default(0), not null
 #  state_id            :integer          default(3), not null
 #  graduation_id       :integer          default(2), not null
-#  academic_program_id :integer
 #  prolong_requested   :boolean          default(FALSE)
 #  prolonged           :boolean          default(FALSE)
 #  prolonged_at        :datetime
@@ -27,7 +26,6 @@ class JobOffer < ActiveRecord::Base
   include Bootsy::Container
   include JobOfferScopes
 
-  ACADEMIC_PROGRAMS = ['bachelor', 'master', 'phd', 'alumnus']
   CATEGORIES = ['traineeship', 'sideline', 'graduate_job', 'working_student']
   STATES = ['ABROAD', 'BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
   GRADUATIONS = ['secondary_education', 'abitur',  'bachelor', 'master', 'phd'] 
@@ -166,10 +164,6 @@ class JobOffer < ActiveRecord::Base
 
   def state
     STATES[state_id]
-  end
-
-  def academic_program
-    ACADEMIC_PROGRAMS[academic_program_id]
   end
 
   def minimum_degree
