0d346440b609fbc44fa0c343984eb6c6210be31e
Pascal Reinhardt <pasrein@web.de>
2014-06-24 15:44:51 +0200

    small header and default locale

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module HpiHiwiPortal
     config.i18n.enforce_available_locales = true
     config.i18n.available_locales = [:en, :de]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :de
   end
 end
