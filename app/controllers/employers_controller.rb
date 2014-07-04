24e3b1c0370283e49e701b9a0fcd2333675412b9
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-07-05 01:04:12 +0200

    solved #464 but new_line doesnt work

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -87,6 +87,6 @@ class EmployersController < ApplicationController
     end
 
     def employer_params
-      params.require(:employer).permit(:name, :description, :avatar, :number_of_employees, :year_of_foundation, :line_of_business, :website, :place_of_business, :requested_package_id, staff_members_attributes: [user_attributes: [:firstname, :lastname, :email, :password, :password_confirmation ]])
+      params.require(:employer).permit(:name, :description, :contact, :avatar, :number_of_employees, :year_of_foundation, :line_of_business, :website, :place_of_business, :requested_package_id, staff_members_attributes: [user_attributes: [:firstname, :lastname, :email, :password, :password_confirmation ]])
     end
 end
