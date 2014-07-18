66fa74aaaf2870e7ff9ed14c7816ed2ceffad169
Pascal Reinhardt <pasrein@web.de>
2014-07-18 14:48:10 +0200

    javascript fix and formatting

--- app/assets/javascripts/job_offers.js
+++ app/assets/javascripts/job_offers.js
@@ -6,13 +6,12 @@ $(document).ready(function($) {
       var category = $('#category_id')
       var selected_category = category.options[category.selectedIndex]
       var can_book = selected_category.getAttribute('can_book')=="true" ? true : false
-      if !can_book
-      {
+      if (!can_book) {
         e.preventDefault();
         $('#book_modal').modal('show')
-      }         
+      }
     });
     $("#book_single").click(function(e) {
-        $("#submit").click()   
+        $("#submit").click()
     });
 });
