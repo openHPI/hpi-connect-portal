62a6f39affd47aff9952d1501dc5d479c066a6dd
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-07-24 04:22:38 +0200

    added employer and staff

--- app/assets/javascripts/job_offers.js
+++ app/assets/javascripts/job_offers.js
@@ -2,11 +2,6 @@ $(document).ready(function($) {
     $(".clickable").click(function() {
         window.document.location = $(this).attr("href");
     });
-    var menus = document.getElementById("site_nav").getElementsByTagName("li")
-    for(var i = 0; i < menus.length; i++)
-    {
-      //menus[i].style.box-shadow = "red"
-    }
     $("#submit").click(function(e) {
       var category = $('#category_id')
       var selected_category = category.options[category.selectedIndex]
