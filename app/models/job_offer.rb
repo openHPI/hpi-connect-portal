c6eff853732c8378ec589a279f16ba9125a30758
alexanderernst <leverkusener77@googlemail.com>
2014-08-17 16:51:36 +0200

    removed validation for starting date

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -47,7 +47,6 @@ class JobOffer < ActiveRecord::Base
 
   validates :title, :description, :employer, :category, :state, :graduation_id, :start_date, presence: true
   validates :compensation, :time_effort, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
-  validates_datetime :start_date, on_or_after: lambda { Date.current }, on_or_after_message: I18n.t("activerecord.errors.messages.in_future")
   validates_datetime :end_date, on_or_after: :start_date, allow_blank: :end_date
 
   self.per_page = 15
