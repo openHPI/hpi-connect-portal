a1731054db8a07b1b9cdfaebc17e7f41fff145a4
Daniel Dummer <daniel.dummer@student.hpi.uni-potsdam.de>
2014-10-04 00:02:05 +0200

    corrected indentation

--- app/controllers/certificate_controller.rb
+++ app/controllers/certificate_controller.rb
@@ -1,14 +1,14 @@
 class CertificateController < ApplicationController
 
-	before_action :set_certificate, only: [:destroy]
+  before_action :set_certificate, only: [:destroy]
 
   def destroy
     if @certificate.destroy
-	  	respond_and_redirect_to(:back, I18n.t('certificates.messages.successfully_deleted'))
+      respond_and_redirect_to(:back, I18n.t('certificates.messages.successfully_deleted'))
     else
       render_errors_and_action(@certificate, 'destroy')
-    end
-	end
+      end
+  end
 
   private
   def set_certificate
