ffbbf54d9c66476ab758f9c9248ede597900c9fd
willi.gierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-10-18 12:23:18 +0200

    fixed mailer and mails

--- app/mailers/job_offers_mailer.rb
+++ app/mailers/job_offers_mailer.rb
@@ -88,7 +88,9 @@ class JobOffersMailer < ActionMailer::Base
   end
 
   def students_by_programming_language(job_offer, students)
-    students & job_offer.programming_languages.map{ |programming_language|
-    ProgrammingLanguagesNewsletterInformation.where("programming_language_id = ?", programming_language.id).map(&:student)}
+    programming_language_students = []
+    job_offer.programming_languages.map{ |programming_language|
+    programming_language_students << ProgrammingLanguagesNewsletterInformation.where("programming_language_id = ?", programming_language.id).map(&:student)}
+    students.to_a & programming_language_students.flatten
   end
 end
