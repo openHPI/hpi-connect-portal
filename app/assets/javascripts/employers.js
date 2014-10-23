b6d9428e11a6670ceccb6144c03819dfd6fdb5dd
willi.gierke <willi.gierke@student.hpi.uni-potsdam.de>
2014-10-24 00:59:27 +0200

    still trying to render a partial star

--- app/assets/javascripts/employers.js
+++ app/assets/javascripts/employers.js
@@ -11,11 +11,11 @@ $(document).ready( function() {
     
     if($('div.star-percent').length > 0) {
         var partial_star = $("div.star-percent").attr("class").match(/star-percent-\d+/g);
-        alert(partial_star);
+        
         var percentage = parseInt(partial_star.toString().split("-")[2])/100;
         var position = Math.round(16-16*percentage) + "px";
         alert($(partial_star).length);
-        $(partial_star).css({ "background-position":  "top right" });
-        alert("ende");
+        $(partial_star).css({ "background-position":  "-8 -8" });
+        
     }
 });
