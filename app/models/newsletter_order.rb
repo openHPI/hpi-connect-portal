3232a22625041bfdc7228919007641da43236e33
alexanderernst <leverkusener77@googlemail.com>
2014-11-16 23:14:52 +0100

    added model validations

--- app/models/newsletter_order.rb
+++ app/models/newsletter_order.rb
@@ -1,5 +1,12 @@
 class NewsletterOrder < ActiveRecord::Base
   belongs_to :student
   serialize :search_params
-  validates :search_params, presence: true
+  validate :search_hash_cannot_be_nil
+  validates :student, presence: true
+
+  def search_hash_cannot_be_nil
+    if search_params.nil?
+      errors.add(:search_params, "can't be nil")
+    end
+  end
 end
--- spec/models/newsletter_order_spec.rb
+++ spec/models/newsletter_order_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe NewsletterOrder do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "does create NewsletterOrders" do
+    assert NewsletterOrder.create(student: FactoryGirl.create(:student), search_params: {state: 3}).valid?
+  end
+
+  it "does not create NewsletterOrders without student" do
+    assert !NewsletterOrder.create(search_params: {state:3}).valid?
+  end
+
+  it "does not create NewsletterOrders without search_params" do
+    assert !NewsletterOrder.create(student: FactoryGirl.create(:student)).valid?
+  end
+
+  it "does create NewsletterOrders with empty Hash" do
+    assert NewsletterOrder.create(student: FactoryGirl.create(:student), search_params: {}).valid?
+  end
 end
