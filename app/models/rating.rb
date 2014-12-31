14fe3707969717490889ac58255f8081fd656e4d
Felix Kubicek <kubicek@uni-potsdam.de>
2014-12-31 16:28:45 +0100

    annotate rating schema

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: ratings
+#
+#  id           :integer          not null, primary key
+#  student_id   :integer
+#  employer_id  :integer
+#  job_offer_id :integer
+#  score        :integer
+#  headline     :text
+#  description  :text
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class Rating < ActiveRecord::Base
   belongs_to :student
   belongs_to :employer
