861b4c2dcfbdecd9fc0c63daee4a191f26a57ca9
Felix Kubicek <kubicek@uni-potsdam.de>
2014-12-31 16:58:17 +0100

    added validations to rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -17,4 +17,9 @@ class Rating < ActiveRecord::Base
   belongs_to :student
   belongs_to :employer
   belongs_to :job_offer
+  
+  validates :student, :employer, :job_offer, :score, :headline, :description, presence: true
+  validates :score, inclusion: { in: 0..5 }
+  
+  
 end
