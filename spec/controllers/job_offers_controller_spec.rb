bfe9fba0dd75d7795c5a48504af27add0dfc498c
Felix Kubicek <kubicek@uni-potsdam.de>
2015-02-24 18:47:51 +0100

    added test cases for job_offers controller (test first)

--- spec/controllers/job_offers_controller_spec.rb
+++ spec/controllers/job_offers_controller_spec.rb
@@ -142,6 +142,40 @@ describe JobOffersController do
       get :index, ({:employer => @employer_one.id}), valid_session
       assigns(:job_offers_list)[:items].to_a.should =~ (job_offers).to_a
     end
+    
+    context "student selects student group" do
+    
+      subject(:hpi_group_id)     { Student::GROUPS.index("hpi") }
+      subject(:dschool_group_id) { Student::GROUPS.index("dschool") }
+      subject(:both_group_id)    { Student::GROUPS.index("both") }
+      
+      let!(:job_offers_hpi)     { [@job_offer] }
+      let!(:job_offers_dschool) { FactoryGirl.create_list(:job_offer, 2, employer: @employer_two, status: @active, student_group_id:  dschool_group_id)}
+      let!(:job_offers_both)    { FactoryGirl.create_list(:job_offer, 2, employer: @employer_three, status: @active, student_group_id: both_group_id)}
+      
+      
+      context "student selects 'HPI' group" do 
+        it "assings all job offers tagged with 'HPI' and 'Both' group to @job_offers_list[:items]" do
+          get :index, ({:student_group => hpi_group_id}), valid_session
+          assigns(:job_offers_list)[:items].to_a.should =~ (job_offers_hpi | job_offers_both).to_a  
+        end
+      end
+      
+      context "student selects 'D-School' group" do
+        it "assings all job offers tagged with with 'D-School' and 'Both' group to @job_offers_list[:items]" do
+          get :index, ({:student_group => dschool_group_id }), valid_session
+          assigns(:job_offers_list)[:items].to_a.should =~ (job_offers_dschool | job_offers_both).to_a
+        end
+      end
+      
+      context "student selects 'Both' group" do
+        it "assings all job offers tagged with 'HPI', 'D-School' and 'Both' group to @job_offers_list[:items]" do 
+          get :index, ({:student_group => both_group_id}), valid_session
+          assigns(:job_offers_list)[:items].to_a.should =~ (job_offers_hpi | job_offers_dschool | job_offers_both).to_a
+        end
+      end
+    end
+    
   end
 
   describe "GET matching" do
