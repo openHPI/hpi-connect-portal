75694ec5488bdd7a7e009364ede0ed91d3b44aad
willi.gierke <willi.gierke@student.hpi.uni-potsdam.de>
2015-03-13 23:43:30 +0100

    fixed tinymce development error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,18 +16,18 @@
 //= require jquery.turbolinks
 //= require jquery-star-rating
 //= require tinymce-jquery
-//= 
+//=
 //= require bootstrap/bootstrap
 //= require_tree .
 
 
 $(document).ready( function() {
     $('.dropdown-toggle').dropdown();
-	
+
 	var language = window.locale === 'de' ? 'de' : 'en';
-	
-    tinymce.baseURL = '/connect/jobportal/assets/tinymce';
-    
+
+    tinymce.baseURL = 'https://www.hpi.uni-potsdam.de/connect/jobportal/assets/tinymce/';
+
 	$('textarea.tinymce').tinymce({
 			language : language,
       plugins: [
@@ -39,24 +39,24 @@ $(document).ready( function() {
 			menubar: "table format view insert edit",
 		    toolbar: "undo redo | styleselect | bold italic | forecolor backcolor | alignleft aligncenter alignright | bullist outdent indent | link | code"
 	});
-  
+
   $('#employers_carousel .item').first().addClass("active");
-  
+
   $('#employers_carousel').carousel({
     interval: 4000,
-    pause: "hover" 
+    pause: "hover"
   });
-  
+
   $('#job_offer_preview_button').click(function() {
-      
-    var job_offer_title = jQuery('.job_offer_title input').val(); 
+
+    var job_offer_title = jQuery('.job_offer_title input').val();
     var job_offer_description = tinymce.activeEditor.getContent();
-    
+
     $('#job_offer_preview_modal').find(".modal-title").text(job_offer_title);
     $('#job_offer_preview_modal').find(".modal-body").html(job_offer_description);
     $('#job_offer_preview_modal').modal('show');
   });
-  
+
 });
 
 $(function() {
