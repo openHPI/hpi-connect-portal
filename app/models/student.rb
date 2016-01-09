65e301e3e8daf6495c8e584d6ead60383b07a563
willi.gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-01-09 17:44:16 +0100

    added graduation entry

--- app/models/student.rb
+++ app/models/student.rb
@@ -172,12 +172,13 @@ class Student < ActiveRecord::Base
   end
 
   def self.export_alumni
-    attributes = %w{lastname firstname alumni_email email}
     CSV.generate(headers: true) do |csv|
+      attributes = %w{lastname firstname alumni_email email graduation}
       csv << attributes
+      attributes.pop()
       find_each do |student|
         if student.user.alumni?
-          csv << attributes.map{ |attr| student.send(attr) }
+          csv << attributes.map{ |attr| student.send(attr)}.push(I18n.t('activerecord.attributes.user.degrees.' + student.graduation))
         end
       end
       csv << [I18n.t('activerecord.attributes.alumni.following_alumni_are_not_registered_yet'), '', '', '']
