f0dee0b96d5df32d8fcb85b7ffe63e86906659a1
willi.gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-02-24 00:00:54 +0100

    upgraded gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.datepicker
+//= require jquery-ui/datepicker
 //= require jquery.turbolinks
 //= require jquery-star-rating
 //= require jquery.raty.min
