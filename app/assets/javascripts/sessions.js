9f963cd76e86ef21af49ec09d9741546f3e51241
veracl <veraclemens7@googlemail.com>
2018-05-30 13:08:55 +0200

    Small fixes

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -17,7 +17,7 @@ $(document).ready( function() {
     }
     activeLink.attr("class", "theme3")
     activeLink.parent().attr("class", "active")
-	
+
     $('#sign_in_form #username_field').bind('input', function() {
         $('#sign_in_form #url_field').val('https://openid.hpi.uni-potsdam.de/user/' + $(this).val());
     });
