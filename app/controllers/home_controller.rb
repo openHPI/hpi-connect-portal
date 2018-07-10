fc60ff5ee414dfccf6276f6a9c928ec49236b3c5
veracl <veraclemens7@googlemail.com>
2018-07-10 16:51:31 +0200

    Fix bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   skip_before_filter :signed_in_user
   skip_before_filter :verify_authenticity_token
 
+  authorize_resource only: [:admin_overview], class: false
+
   def index
     @job_offers = JobOffer.active.last(5).reverse
     @employers = Employer.active.last(5).reverse
