5540821da61a2bfbf544dea0c3a9b8a83f15cbb5
Vera Clemens <mail@veraclemens.org>
2019-03-02 18:35:28 +0100

    Add pending package and prolongation requests to admin overview

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,8 +10,10 @@ class HomeController < ApplicationController
   end
 
   def admin_overview
-    @job_offers = JobOffer.pending.where('created_at >= ?', 2.week.ago).order(created_at: :desc)
-    @employers = Employer.pending.where('created_at >= ?', 2.week.ago).order(created_at: :desc)
+    @job_offers_pending_activation = JobOffer.pending.where('created_at >= ?', 2.week.ago).order(created_at: :desc)
+    @job_offers_pending_prolongation = JobOffer.where('prolong_requested = true AND updated_at >= ?', 2.week.ago).order(created_at: :desc)
+    @employers_pending_activation = Employer.pending.where('created_at >= ?', 2.week.ago).order(created_at: :desc)
+    @employers_pending_package = Employer.where('requested_package_id != booked_package_id AND updated_at >= ?', 2.week.ago).order(created_at: :desc)
     flash.now[:notice] = I18n.t('home.admin_overview.notice')
   end
 
