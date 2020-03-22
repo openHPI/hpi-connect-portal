a68f21831f63f832339f02431060eb3b0933ce7b
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2020-03-23 00:13:39 +0100

    Show external website links with correct hrefs

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -3,4 +3,7 @@ module UrlHelper
   def url_with_protocol(url)
     /^http/.match(url) ? url : "http://#{url}"
   end
+  def url_without_protocol(url)
+  	/^http/.match(url) && /\/\//.match(url) ? url.slice(url.index("//")+2..-1) : url
+  end
 end
