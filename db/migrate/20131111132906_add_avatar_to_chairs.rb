44a890d2e3d956aa46af0ff5fdfa960cf1055f27
Christian Willems <christian.willems@hpi.de>
2023-10-11 18:11:41 +0200

    Fix paperclip migrations

--- db/migrate/20131111132906_add_avatar_to_chairs.rb
+++ db/migrate/20131111132906_add_avatar_to_chairs.rb
@@ -1,9 +1,15 @@
 class AddAvatarToChairs < ActiveRecord::Migration[4.2]
   def up
-		add_attachment :chairs, :avatar
+    add_column :chairs, :avatar_file_name, :string
+    add_column :chairs, :avatar_file_size, :integer
+    add_column :chairs, :avatar_content_type, :string
+    add_column :chairs, :avatar_updated_at, :datetime
   end
 
-	def down 
-		remove_attachment :chairs, :avatar
-	end
+  def down 
+    remove_column :chairs, :avatar_file_name, :string
+    remove_column :chairs, :avatar_file_size, :integer
+    remove_column :chairs, :avatar_content_type, :string
+    remove_column :chairs, :avatar_updated_at, :datetime
+  end
 end
--- db/migrate/20140722190743_add_attachment_offer_as_pdf_to_job_offers.rb
+++ db/migrate/20140722190743_add_attachment_offer_as_pdf_to_job_offers.rb
@@ -1,11 +1,15 @@
 class AddAttachmentOfferAsPdfToJobOffers < ActiveRecord::Migration[4.2]
   def self.up
-    change_table :job_offers do |t|
-      t.attachment :offer_as_pdf
-    end
+    add_column :job_offers, :offer_as_pdf_file_name, :string
+    add_column :job_offers, :offer_as_pdf_file_size, :integer
+    add_column :job_offers, :offer_as_pdf_content_type, :string
+    add_column :job_offers, :offer_as_pdf_updated_at, :datetime
   end
 
   def self.down
-    drop_attached_file :job_offers, :offer_as_pdf
+    remove_column :job_offers, :offer_as_pdf_file_name, :string
+    remove_column :job_offers, :offer_as_pdf_file_size, :integer
+    remove_column :job_offers, :offer_as_pdf_content_type, :string
+    remove_column :job_offers, :offer_as_pdf_updated_at, :datetime
   end
 end
